83aed9b4045d3c4eb0acb02c44f9350f5afa7f10
Marc Oller <marc.om@icloud.com>
2025-10-14 21:33:13 +0200

    chore: update bookmarks

--- src/components/photo-grid.tsx
+++ src/components/photo-grid.tsx
@@ -1,24 +1,24 @@
-import type { Photo } from "../types"
-import { PhotoItem } from "./photo-item"
-import '../css/photo-grid.css'
+// import type { Photo } from "../types"
+// import { PhotoItem } from "./photo-item"
+// import '../css/photo-grid.css'
 
-interface PhotoGridProps {
-  photos: Photo[]
-  columns: number
-  onPhotoClick: (photo: Photo) => void;
-}
+// interface PhotoGridProps {
+//   photos: Photo[]
+//   columns: number
+//   onPhotoClick: (photo: Photo) => void;
+// }
 
-export const PhotoGrid = ({ photos, columns, onPhotoClick }: PhotoGridProps) => {
-  return (
-    <div
-      className="photo-grid"
-      style={{
-        gridTemplateColumns: `repeat(${columns}, 1fr)`,
-      }}
-    >
-      {photos.map(photo => (
-        <PhotoItem key={photo.id} photo={photo} onPhotoClick={onPhotoClick} />
-      ))}
-    </div>
-  )
-}
+// export const PhotoGrid = ({ photos, columns, onPhotoClick }: PhotoGridProps) => {
+//   return (
+//     <div
+//       className="photo-grid"
+//       style={{
+//         gridTemplateColumns: `repeat(${columns}, 1fr)`,
+//       }}
+//     >
+//       {photos.map(photo => (
+//         <PhotoItem key={photo.id} photo={photo} onPhotoClick={onPhotoClick} />
+//       ))}
+//     </div>
+//   )
+// }
